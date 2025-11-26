aae6517cba0e3cf6176fbb8a4fd616ea9ac836d1
Markus Eisenbach <meis@gurla.net>
2025-11-26 16:53:37 -0500

    writing greens function

--- src/SingleSite/writeSingleAtomData_pottxt.cpp
+++ src/SingleSite/writeSingleAtomData_pottxt.cpp
@@ -1,33 +1,35 @@
 #include <hdf5.h>
 #include <string.h>
 
+#include <cstdio>
+#include <cstdlib>
+
 #include "AtomData.hpp"
 #include "writeSingleAtomData.hpp"
 
 int writeSingleAtomData_pottxt(const char *fname, AtomData &atom)
 {
-  char gname[128];
-
-  fout = open("fname", "w");
+  FILE* fout = std::fopen(fname, "w");
 
   fprintf(fout, "HEADER\n%.80s\n", &atom.header[0]);
   fprintf(fout, "Z\n%f\n", atom.ztotss);
   fprintf(fout, "ZCORE\n%f\n", atom.zcorss);
   fprintf(fout, "NSPIN\n%d\n", atom.nspin);
   fprintf(fout, "VDIF\n%f\n", atom.vdif);
-  fprintf(fout, "ALAT\n%f\n", atom.ztotss);
+  fprintf(fout, "ALAT\n%f\n", atom.alat);
   fprintf(fout, "EFERMI\n%f\n", atom.efermi);
 
+  fprintf(fout, "GRID\nLOG\n");
   fprintf(fout, "XSTART\n%f\n", atom.xstart);
   fprintf(fout, "XMT\n%f\n", log(atom.rmt));
-  fprintf(fout, "JMT\n%f\n", atom.jmt);
-  fprintf(fout, "JWS\n%f\n", atom.jws);
+  fprintf(fout, "JMT\n%d\n", atom.jmt);
+  fprintf(fout, "JWS\n%d\n", atom.jws);
 
-  fprintf(fout, "NUMC\n%f\n", atom.numc);
+  fprintf(fout, "NUMC\n%d\n", atom.numc);
 
   for(int ns=0; ns<atom.nspin; ns++)
   {
-    fprintf(fout, "SPIN\n%d\n", ns);
+    fprintf(fout, "SPIN\n%d\n", ns+1);
 
     fprintf(fout, "RPOT\n");
     for(int i=0; i<atom.jmt; i++)
@@ -44,12 +46,12 @@ int writeSingleAtomData_pottxt(const char *fname, AtomData &atom)
       for(int i=0; i<atom.numc; i++)
         fprintf(fout, "%2d %2d %2d  %f\n", atom.nc(i,ns), atom.lc(i,ns), atom.kc(i,ns), atom.ec(i,ns));
     }
-    fprintf(fout, "XVALWS\n%f\n", atom.xvalws[i]);
+    fprintf(fout, "XVALWS\n%f\n", atom.xvalws[ns]);
   }
   //  for(int j=0; j<100; j++) gname[j]=' ';
   //  write_vector<char>(loc_id,"JTitle",&gname[0],80);
   //  write_vector<double>(loc_id,"evec",&atom.evec[0],3);
 
-  close(fout);
+  std::fclose(fout);
   return 0;
 }
